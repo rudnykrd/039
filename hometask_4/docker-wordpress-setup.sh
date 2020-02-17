9289b989922faa47e3e9b579e5d0af9d870a501d
Roman Rudnyk <rudnykrd@gmail.com>
2020-02-18 01:37:18 +0200

    created Vagrantfile for installing Docker on Ubuntu, hosting two containers: Wordpress CMS and MySQL server

---
+++ hometask_4/docker-wordpress-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+usermod -aG docker vagrant
+docker network create wpnet
+docker run --name db -e MYSQL_DATABASE=wordpress -e MYSQL_USER=username -e MYSQL_PASSWORD=password --network wpnet -d rudnykrd/db:latest
+docker run --name wp --link db:dbhost -p 80:80 -p 443:443 --network wpnet -d rudnykrd/wp-https:latest
